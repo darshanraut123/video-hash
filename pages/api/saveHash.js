9b0cfdb09a32c9ff039894ff27c1735f3d692bdd
darshanraut123 <darshanraut3828@gmail.com>
2024-10-07 23:57:13 +0530

    save video hash api

---
+++ pages/api/saveHash.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const uri =
+  "mongodb+srv://darshanraut123:darshanraut123@cluster0.blxpgv4.mongodb.net/";
+const client = new MongoClient(uri);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    // Connect to MongoDB
+    await client.connect();
+    const database = client.db("video-hash"); // Replace with your database name
+    const collection = database.collection("videoHashRecords");
+    console.log("body ===>  " + req.body);
+    const body = req.body;
+    const bodyObj = JSON.parse(body);
+    // Insert the fingerprint and JSON data into the collection
+    const result = await collection.insertOne(bodyObj);
+    res.status(200).json(result);
+  }
+}
