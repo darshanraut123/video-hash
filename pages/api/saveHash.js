401b9b8a667c837cc9020b9e4de5a3e63dd0321d
darshanraut123 <darshanraut3828@gmail.com>
2024-10-11 02:06:29 +0530

    API find hash

--- pages/api/saveHash.js
+++ pages/api/saveHash.js
@@ -5,15 +5,20 @@ const uri =
 const client = new MongoClient(uri);
 
 export default async function handler(req, res) {
+  await client.connect();
+  const database = client.db("video-hash"); // Replace with your database name
+  const collection = database.collection("videoHashRecords");
   if (req.method === "POST") {
-    // Connect to MongoDB
-    await client.connect();
-    const database = client.db("video-hash"); // Replace with your database name
-    const collection = database.collection("videoHashRecords");
     console.log("body ===>  " + req.body);
     const body = req.body;
     // Insert the fingerprint and JSON data into the collection
-    const result = await collection.insertOne(body);
+    const result = await collection.insertOne({ _id: body.videoId, ...body });
     res.status(200).json(result);
+  } else if (req.method === "GET") {
+    const { videoId } = req.query;
+    if (!videoId) res.status(404).json({ message: "not found" });
+    const document = await collection.findOne({ videoId: videoId });
+    if (document) res.status(200).json({ document, message: "found" });
+    else res.status(200).json({ message: "not found" });
   }
 }
