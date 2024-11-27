64bec72a8cd25baf0b46a909d0e0529bb7347154
darshanraut123 <darshanraut3828@gmail.com>
2024-11-28 02:18:06 +0530

    Added photo api

--- pages/api/savePhotoHash.js
+++ pages/api/savePhotoHash.js
@@ -8,17 +8,17 @@ export default async function handler(req, res) {
   try {
     await client.connect();
     const database = client.db("video-hash"); // Replace with your database name
-    const collection = database.collection("videoHashRecords");
+    const collection = database.collection("photoHashRecords");
     if (req.method === "POST") {
       console.log("body ===>  " + req.body);
       const body = req.body;
       // Insert the fingerprint and JSON data into the collection
-      const result = await collection.insertOne({ _id: body.videoId, ...body });
+      const result = await collection.insertOne({ _id: body.photoId, ...body });
       res.status(200).json(result);
     } else if (req.method === "GET") {
-      const { videoId } = req.query;
-      if (!videoId) res.status(404).json({ message: "not found" });
-      const document = await collection.findOne({ videoId: videoId });
+      const { photoId } = req.query;
+      if (!photoId) res.status(404).json({ message: "not found" });
+      const document = await collection.findOne({ photoId: photoId });
       if (document) res.status(200).json({ document, message: "found" });
       else res.status(200).json({ message: "not found" });
     }
