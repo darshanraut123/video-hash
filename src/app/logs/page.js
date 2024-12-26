461d1bcc4ee6c0ce2a1f2f70ce7e9d157e914b08
darshanraut123 <darshanraut3828@gmail.com>
2024-12-26 22:49:23 +0530

    Data fix

--- src/app/logs/page.js
+++ src/app/logs/page.js
@@ -13,17 +13,27 @@ import {
   Box,
   IconButton,
   Tooltip,
+  CircularProgress,
 } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 
 const VerifierTable = () => {
-  const [data, setData] = React.useState([]);
-
+  const [logs, setLogs] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
   React.useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("http://localhost:4000/api/verifylogs");
-      const data = await response.json();
-      setData(data.logs);
+      try {
+        setLoading(true);
+        const response = await fetch("http://localhost:4000/api/verifylogs");
+        const data = await response.json();
+        console.log(data.logs);
+        data.logs && setLogs(data.logs);
+      } catch (e) {
+        console.log(e);
+        setLogs([]);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
@@ -33,86 +43,91 @@ const VerifierTable = () => {
       <Typography variant="h4" gutterBottom>
         Verify Logs
       </Typography>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="verifier table">
-          <TableHead>
-            <TableRow>
-              <TableCell>#</TableCell>
-              <TableCell>Created</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Message</TableCell>
-              <TableCell>URI</TableCell>
-              <TableCell>ID</TableCell>
-              <TableCell>Verify Hashes</TableCell>
-              <TableCell>Original Hashes</TableCell>
-              <TableCell>Average Distance</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((item, index) => (
-              <TableRow key={item._id}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{item.createdAt}</TableCell>
-                <TableCell>{item.verifierEmail}</TableCell>
-                <TableCell>{item.verifierName}</TableCell>
-                <TableCell>{item.message}</TableCell>
-                <TableCell>
-                  <Tooltip title={item.uri}>
-                    <IconButton>
-                      <InfoIcon />
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
-                <TableCell>{item.videoId ? item.videoId : "NA"}</TableCell>
-                <TableCell>
-                  <Tooltip
-                    title={
-                      <div>
-                        {item.verifyVideoHashes
-                          ? item.verifyVideoHashes.map((hash, idx) => (
-                              <Typography key={idx} variant="body2">
-                                {hash},
-                              </Typography>
-                            ))
-                          : "NA"}
-                      </div>
-                    }
-                    arrow
-                  >
-                    <IconButton>
-                      <InfoIcon />
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
-                <TableCell>
-                  <Tooltip
-                    title={
-                      <div>
-                        {item.originalVideoHashes
-                          ? item.originalVideoHashes.map((hash, idx) => (
-                              <Typography key={idx} variant="body2">
-                                {hash},
-                              </Typography>
-                            ))
-                          : "NA"}
-                      </div>
-                    }
-                    arrow
-                  >
-                    <IconButton>
-                      <InfoIcon />
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
-                <TableCell>
-                  {item.averageDistance ? item.averageDistance.toFixed(2) : 0}%
-                </TableCell>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="verifier table">
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Created</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Message</TableCell>
+                <TableCell>URI</TableCell>
+                <TableCell>ID</TableCell>
+                <TableCell>Verify Hashes</TableCell>
+                <TableCell>Original Hashes</TableCell>
+                <TableCell>Average Distance</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {logs.map((item, index) => (
+                <TableRow key={item._id}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{item.createdAt}</TableCell>
+                  <TableCell>{item.verifierEmail}</TableCell>
+                  <TableCell>{item.verifierName}</TableCell>
+                  <TableCell>{item.message}</TableCell>
+                  <TableCell>
+                    <Tooltip title={item.uri}>
+                      <IconButton>
+                        <InfoIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell>{item.videoId ? item.videoId : "NA"}</TableCell>
+                  <TableCell>
+                    <Tooltip
+                      title={
+                        <div>
+                          {item.verifyVideoHashes
+                            ? item.verifyVideoHashes.map((hash, idx) => (
+                                <Typography key={idx} variant="body2">
+                                  {hash},
+                                </Typography>
+                              ))
+                            : "NA"}
+                        </div>
+                      }
+                      arrow
+                    >
+                      <IconButton>
+                        <InfoIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell>
+                    <Tooltip
+                      title={
+                        <div>
+                          {item.originalVideoHashes
+                            ? item.originalVideoHashes.map((hash, idx) => (
+                                <Typography key={idx} variant="body2">
+                                  {hash},
+                                </Typography>
+                              ))
+                            : "NA"}
+                        </div>
+                      }
+                      arrow
+                    >
+                      <IconButton>
+                        <InfoIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell>
+                    {item.averageDistance ? item.averageDistance.toFixed(2) : 0}
+                    %
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </Box>
   );
 };
