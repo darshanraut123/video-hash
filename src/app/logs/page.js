ed318d669999f24b62919a82cf48efc2471fcb5c
darshanraut123 <darshanraut3828@gmail.com>
2024-12-26 22:11:27 +0530

    Added created at date change

--- src/app/logs/page.js
+++ src/app/logs/page.js
@@ -20,30 +20,12 @@ const VerifierTable = () => {
   const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
-    const data = [
-      {
-        _id: "676d63ba500a2b7019de49a7",
-        verifierEmail: "darshan.raut@buzzybrains.com",
-        verifierName: "Darshan Raut",
-        message: "Found a match",
-        uri: "file:///data/user/0/com.videohash/cache/rn_image_picker_lib_temp_a23bdce1-54f7-44a2-85c0-87b7e62662fd.mp4",
-        videoId: "17346663928731",
-        verifyVideoHashes: [
-          "1010100101010110101011111010111101010111011111111010111101011111",
-          "1000011101111111100011111111111101111111011111111111111101111111",
-          "1110111101111111111011111011111101111111111111111111111101111111",
-          "1001111101111111101111111011111101111111011111111111111101111111",
-        ],
-        originalVideoHashes: [
-          "1010110101011010101011011010111101011111010111111010110110111111",
-          "1000011101111111100111111100011101111111010111111111111111011111",
-          "1110011100111111111001111011111101111111011111111011111111111111",
-          "1001111101111111111111111011111101111111011111111111111111111111",
-        ],
-        averageDistance: 90.234375,
-      },
-    ];
-    setData(data);
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:4000/api/verifylogs");
+      const data = await response.json();
+      setData(data.logs);
+    };
+    fetchData();
   }, []);
 
   return (
